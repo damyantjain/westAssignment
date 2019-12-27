46198eca656da17d5e764d5bf43444347352c88d
Damyant Jain <35915257+damyantjain@users.noreply.github.com>
2019-12-27 18:30:05 +0530

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LoginMVC.Models;
+using System.Data.SqlClient;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LoginMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        [HttpGet]
+        // GET: /<controller>/
+        public IActionResult Login()
+        {
+            return View();
+        }
+        void ConnectionString()
+        {
+            con.ConnectionString = "host=db4free.net;database=demowest;uid=demowest;password=Damyant@580;";
+        }
+        public IActionResult Verify(Account acc)
+        {
+            ConnectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from logintbl where username='"+acc.Name+"' and password='"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+
+
+            
+
+            
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace LoginMVC.Models
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
